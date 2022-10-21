d5e8e9c05b37e56315782447b21f60e4d89bff34
pokemon2808 <dedeaqzhar12@gmail.com>
2022-10-21 11:09:38 +0800

    hari 5

---
+++ operator penugasan.py
@@ -0,0 +1,18 @@
+nilai = 2
+nilai2 = 50
+nilai +=3
+nilai2 =+3
+print(int(nilai))
+print(int(nilai2))
+nilai -= 2
+print(int(nilai))
+nilai *=8
+print(int(nilai))
+nilai /=2
+print(int(nilai))
+nilai %=5
+print(int(nilai))
+nilai **=6
+print(int(nilai))
+nilai //= 4
+print(int(nilai))
