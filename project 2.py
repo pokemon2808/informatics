90c0cad144b7ab04fa3d25cf6851585c8e6ff7e6
pokemon2808 <dedeaqzhar12@gmail.com>
2022-10-24 19:19:11 +0800

    hari 6

---
+++ project 2.py
@@ -0,0 +1,18 @@
+nilai =int(input("masukkan nilai : "))
+if nilai >86:
+    print("nilai anda A")
+elif nilai >80:
+    print("nilai anda A-")
+elif nilai >76:
+    print("nilai b+")
+elif nilai >70:
+    print("nilai b")
+elif nilai >66:
+    print("nilai b-")
+elif nilai >60:
+    print("nilai c")
+elif nilai >50:
+    print("nilai D")
+else:
+    print("nilai E")
+print("nilai")
