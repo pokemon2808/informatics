8ff9c284bda16ac0a002cb42baff73bcceef88bc
pokemon2808 <dedeaqzhar12@gmail.com>
2022-10-25 23:19:42 +0800

    hari 7

---
+++ project 4.py
@@ -0,0 +1,8 @@
+nilai =85
+if nilai > 100:
+    print("cetak ini jika kondisi benar")
+elif nilai % 2 == 0:
+    print("print cetak ini jika kondisi elif dijalankan")
+else:
+    print("cetak ini jika kondisi salah")
+print(nilai)
