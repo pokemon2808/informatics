b07ec457c79769728ce0ff473b80d2568d24a85f
pokemon2808 <dedeaqzhar12@gmail.com>
2022-10-27 19:53:21 +0800

    hari ke 9

---
+++ project 5.py
@@ -0,0 +1,11 @@
+umur = int(input("masukkan umur"))
+if (umur > 18 and umur <30) :
+    print("sudah beranjak dewasa")
+elif ( umur >= 30 and umur <45):
+    print("masa masa emas")
+elif ( umur >= 45 and umur < 55):
+    print(" masa paruh baya")
+elif ( umur >= 55):
+    print("masa manula")
+else:
+    print(" masa dibawah umur")
