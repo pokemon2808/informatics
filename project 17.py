580549c2b8fe8bfcc93265adbe61e9f3813051c6
pokemon2808 <dedeaqzhar12@gmail.com>
2022-11-09 22:34:22 +0800

    hari 19

---
+++ project 17.py
@@ -0,0 +1,7 @@
+def printme(stri):
+    "saya sedang  menjalankan sebuah cafe"
+    print (str)
+    return ;
+
+print("saya sedang membangun sebuah cafe yang besar")
+print("dan saya sedang mencari karyawan untuk cafe saya")
