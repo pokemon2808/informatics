fe2acd929c786bc25c1cd57ebf2cd099b26e7b12
pokemon2808 <dedeaqzhar12@gmail.com>
2022-11-15 23:07:01 +0800

    hari 22

---
+++ project 22.py
@@ -0,0 +1,11 @@
+def run():
+    nama = input(" input nama ")
+    alamat = input(" input alamat :")
+    ttl = input("input tempat dan tanggal lahir : ")
+    status = input(" input status")
+    pekerjaan = input("input pekerjaan")
+
+print("===============================================")
+print("biodata anda adalah: ")
+print(' nama: '+("input nama"))
+print("alamat: ")
