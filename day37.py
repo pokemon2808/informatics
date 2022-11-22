49ffc82788603d4c9983c0f32517402fdd1870e8
pokemon2808 <dedeaqzhar12@gmail.com>
2022-11-23 01:05:03 +0800

    day 37

---
+++ day37.py
@@ -0,0 +1,8 @@
+#menentukan bilangan ganjil dan genap
+
+angka = int(input("masukkan blangan :"))
+
+if angka % 2 ==0:
+    print("%d merupakan bilangan genap" % angka)
+else:
+    print("%d merupakan bilangan ganjil " % angka)
