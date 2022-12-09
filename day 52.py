3c83a18723949909a2bc54af1003b5e032db13c6
pokemon2808 <dedeaqzhar12@gmail.com>
2022-12-09 23:33:48 +0800

    day 52

---
+++ day 52.py
@@ -0,0 +1,21 @@
+# program python if else bersarang
+
+print(" Rencana setelah lulus sekolah ?\n1.kuliah\n2.kerja")
+
+jawaban =input(" masukkan pilihan ")
+if jawaban==1:
+    print(" kuliah program studi apa ?\n1. Teknik informatika\n2.akutansi ")
+    jawab=input("masukkan pilihan")
+    if jawab==1 :
+        print(" kuliah di program studi Teknik informatika")
+    else:
+        print(" kuliah di program studi  Akutansi")
+else:
+    print(" kerja di indsutri apa ?\n1. Teknologi\n2. Perbankan ")
+    jawab=input(" masukkan pilihan ")
+    if jawab==1:
+        print(" kerja di industri Teknologi ")
+    else:
+        print(" kerja di industri perbankan ")
+
+
