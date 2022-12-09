9174c851dcb62043e81e9cbf931d2f8d1163e6ab
pokemon2808 <dedeaqzhar12@gmail.com>
2022-12-09 23:35:00 +0800

    day 53

---
+++ day 53.py
@@ -0,0 +1,11 @@
+# program python if else
+
+nilai = input(" masukkan nilai: ")
+if nilai >=90:
+    print(" kategori beasiswa penuh ")
+elif nilai >=80:
+    print(" kategori beasiswa 1")
+elif nilai >=70:
+    print(" kategori beasiswa 2 ")
+else:
+    print(" tidak mendapatkan beasiswa ")
