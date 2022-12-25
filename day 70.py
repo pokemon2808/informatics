1c34a7c1eda401cf5511212f7dfff99bbe6d484d
pokemon2808 <dedeaqzhar12@gmail.com>
2022-12-25 16:03:26 +0800

    day 70

---
+++ day 70.py
@@ -0,0 +1,10 @@
+#bona pergi ke toko beli tinta bollpoint harga satu buah tinta 1.820
+#jika bona membeli 1 lusin  tinta boolpoint dan ia membayar lima lembar uang 5.000
+#berapa kembaliannya
+
+uang = 5000
+banyak_uang = 5
+harga_tinta = 1820
+jumlah_tinta = 12
+
+print(" kembalian bona adalah :", uang * banyak_uang -(harga_tinta * jumlah_tinta))
