e5f8d408ad3cdc881b69a8eb9f830fc7e8ae5eeb
pokemon2808 <dedeaqzhar12@gmail.com>
2022-12-29 23:18:07 +0800

    day 74

---
+++ day 74.py
@@ -0,0 +1,13 @@
+# program fungsi dan perulangan ( mencari luas segitiga )
+pilih = "tidak"
+while pilih =="tidak":
+    def luas_persegi (sisi):
+        luas = sisi * sisi
+        print( " luas sisi persegi adalah :",luas)
+    luas_persegi(25)
+    pilih = input(" apakah anda akhiri ? ya/tidak")
+else:
+    print(" akhiri program : ")
+
+
+
