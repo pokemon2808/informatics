94ff443c3296338093e0306c702c71b0e37a8477
pokemon2808 <dedeaqzhar12@gmail.com>
2022-12-31 23:27:17 +0800

    day 75

---
+++ day 75.py
@@ -0,0 +1,15 @@
+#silvi punya uang 50000
+# silivi naik angkot, ongkos angkot 5000
+# dikampus ditrakitr 3 teman, seharga 15000
+# sisa uang silvi
+
+uang = 50000
+ongkos = 5000
+teman = 3
+jumlah_teman = 15000
+
+sisa_uang = uang - ongkos
+total = teman * jumlah_teman
+
+tersisa = sisa_uang - total
+print(" sisa uang silvi adalah :",tersisa)
