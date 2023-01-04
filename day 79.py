e938b8ad910174c869c4e1c6259ad25a54dd7a2c
pokemon2808 <dedeaqzhar12@gmail.com>
2023-01-04 23:27:31 +0800

    day 79

---
+++ day 79.py
@@ -0,0 +1,9 @@
+# program python  keliling lingkarang
+
+phi = 3.14
+
+lingkarang = int(input(" masukkan panjanng jari jari :"))
+
+keliling = 2 * phi *lingkarang
+
+print(" keliling lingkarang adalah : ",format(keliling))
