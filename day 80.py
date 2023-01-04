fbefa1581aa155dc45534d30d1994f85b084daf2
pokemon2808 <dedeaqzhar12@gmail.com>
2023-01-04 23:28:10 +0800

    day 80

---
+++ day 80.py
@@ -0,0 +1,13 @@
+# program python
+# membuat modul matematika
+
+import matematika
+
+hasi_tambah = matematika.tambah(1,2,3,4,5)
+print(f"hasil tambah ={hasi_tambah}")
+
+hasi_kali = matematika.kali(1,2,3,4,5)
+print(f"hasil kali  ={hasi_kali}")
+
+pangkat_3 = matematika.pangkat(3)
+print(f"hasil pangkat 3 adalah {pangkat_3(3)}")
